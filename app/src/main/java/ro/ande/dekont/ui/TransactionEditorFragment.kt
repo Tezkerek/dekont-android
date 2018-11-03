747716e4a3e9fccec7b3c2899444d88ec29ad2a1
Tezkerek <andrei.ancuta@gmail.com>
2018-11-03 21:21:10 +0200

    WIP transaction editor fragment

---
+++ app/src/main/java/ro/ande/dekont/ui/TransactionEditorFragment.kt
@@ -0,0 +1,37 @@
+package ro.ande.dekont.ui
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ro.ande.dekont.vo.Transaction
+
+import ro.ande.dekont.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A [Fragment] for editing a [Transaction].
+ *
+ */
+class TransactionEditorFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transaction_editor, container, false)
+    }
+
+    companion object {
+        const val ARG_ACTION = "ACTION"
+
+        const val ACTION_CREATE = 0
+        const val ACTION_EDIT = 1
+    }
+
+}
