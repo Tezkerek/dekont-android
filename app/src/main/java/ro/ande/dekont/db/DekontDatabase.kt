ad1a48b7593eacc80185b2a4aae766cbd062b159
Tezkerek <andrei.ancuta@gmail.com>
2018-12-26 13:04:10 +0200

    Reset database schema version to 1

--- app/src/main/java/ro/ande/dekont/db/DekontDatabase.kt
+++ app/src/main/java/ro/ande/dekont/db/DekontDatabase.kt
@@ -7,7 +7,7 @@ import ro.ande.dekont.vo.Transaction
 
 @Database(
         entities = [Transaction::class],
-        version = 3
+        version = 1
 )
 @TypeConverters(Converters::class)
 abstract class DekontDatabase : RoomDatabase() {
